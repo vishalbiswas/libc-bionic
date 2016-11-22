11210c1662ba5b2f3d497d87a5400cacf287b312
Vishal Biswas <vshlbiswas@ymail.com>
2016-11-23 01:06:24 +0530

    Fingers crossed

--- build.sh
+++ build.sh
@@ -77,7 +77,7 @@ if [[ "$skipbenches" == 'yes' ]]
     find bionic -type d -name 'benchmarks' -exec rm -r {} +
 fi
 
-for patch in "$scriptdir/*.patch"
+for patch in $scriptdir/*.patch
   do
     patch -f -p1 < "$patch" || true
 done
